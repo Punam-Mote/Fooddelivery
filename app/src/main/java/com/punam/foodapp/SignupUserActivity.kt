b9030ac96eeca3103849b73de819e464f3b0f0c1
“[Punam-mote]” <[akanshya.chetri786@gmail.com]>
2021-01-19 18:12:37 +0545

    Signup dashboard

---
+++ app/src/main/java/com/punam/foodapp/SignupUserActivity.kt
@@ -0,0 +1,11 @@
+package com.punam.foodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_user)
+    }
+}
