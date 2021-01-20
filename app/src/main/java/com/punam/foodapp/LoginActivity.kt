17bbd9e9aa620f8788520ed662555b7a51eef120
“[Punam-mote]” <[akanshya.chetri786@gmail.com]>
2021-01-20 23:24:09 +0545

    with login and signup

---
+++ app/src/main/java/com/punam/foodapp/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.punam.foodapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var btnLogin:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        btnLogin=findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val intent= Intent(this,DashboardActivity::class.java)
+
+        }
+    }
+}
--- app/src/main/java/com/punam/foodapp/SignupUserActivity.kt
+++ app/src/main/java/com/punam/foodapp/SignupUserActivity.kt
@@ -1,11 +1,19 @@
 package com.punam.foodapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignupUserActivity : AppCompatActivity() {
+    private lateinit var btnRegister: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_user)
+        btnRegister=findViewById(R.id.btnRegister)
+
+        btnRegister.setOnClickListener {
+            val intent= Intent(this, LoginActivity::class.java)
+        }
     }
 }
