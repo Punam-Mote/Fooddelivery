3d11e9ad9e9d8ba261c5cfd15b7e616fdce8aaee
“[Punam-mote]” <[akanshya.chetri786@gmail.com]>
2021-01-22 14:43:43 +0545

    with dashboard

--- app/src/main/java/com/punam/foodapp/LoginActivity.kt
+++ app/src/main/java/com/punam/foodapp/LoginActivity.kt
@@ -4,18 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var btnLogin:Button
+    private lateinit var tvSignup:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         btnLogin=findViewById(R.id.btnLogin)
+        tvSignup=findViewById(R.id.tvSignup)
 
         btnLogin.setOnClickListener {
             val intent= Intent(this,DashboardActivity::class.java)
             startActivity(intent)
-
+        }
+        tvSignup.setOnClickListener {
+            val intent=Intent(this,SignupUserActivity::class.java)
+            startActivity(intent)
         }
     }
 }
