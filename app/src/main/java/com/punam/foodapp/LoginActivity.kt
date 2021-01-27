bc59cddda0344065e0b3ac5d90652cebef2c3c47
“[Punam-mote]” <[akanshya.chetri786@gmail.com]>
2021-01-27 17:03:08 +0545

    applied query for User Data Access Model

--- app/src/main/java/com/punam/foodapp/LoginActivity.kt
+++ app/src/main/java/com/punam/foodapp/LoginActivity.kt
@@ -6,6 +6,13 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.punam.foodapp.db.UserDB
+import com.punam.foodapp.entity.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var etEmail: EditText
@@ -26,5 +33,23 @@ class LoginActivity : AppCompatActivity() {
         tvSignup.setOnClickListener {
             startActivity(Intent(this@LoginActivity, SignupUserActivity::class.java))
         }
+    }
+    private fun login() {
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+
+        var user: User? = null
+        CoroutineScope(Dispatchers.IO).launch {
+            user = UserDB.getInstance(this@LoginActivity)
+                    .getUserDAO()
+                    .checkUser(email, password)
+            if (user == null) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@LoginActivity, "Invalid Username or Password", Toast.LENGTH_SHORT)
+                            .show()
+                }
+            }
+        }
+
     }
 }
