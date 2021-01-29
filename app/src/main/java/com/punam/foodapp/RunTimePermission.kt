52309e41422bace2d6ec436d908f296965bbee8d
“[Punam-mote]” <[akanshya.chetri786@gmail.com]>
2021-01-29 14:03:37 +0545

    Run Time Permission Completed

--- app/src/main/java/com/punam/foodapp/RunTimePermission.kt
+++ app/src/main/java/com/punam/foodapp/RunTimePermission.kt
@@ -1,7 +1,9 @@
 package com.punam.foodapp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class RunTimePermission : AppCompatActivity() {
     private val permissions = arrayOf(
@@ -12,5 +14,30 @@ class RunTimePermission : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_run_time_permission)
+        // Check for permission
+        if (!hasPermission()) {
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this@RunTimePermission,
+            permissions, 777
+        )
+    }
+
+    private fun hasPermission(): Boolean {
+        var hasPermission = true
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = false
+            }
+        }
+        return hasPermission
     }
 }
